f9adc39884c9854746708dc367f0f0324fac6644
Jennifer Ye <jenniferaiye@gmail.com>
2020-05-02 16:52:31 -0400

    Add files via upload

--- book-api.js
+++ book-api.js
@@ -5,39 +5,84 @@ const cors = require('cors');
 const app = express()
 const port = 8000
 
-let books = [{
-    "isbn": "9781593275846",
-    "title": "Eloquent JavaScript, Second Edition",
-    "author": "jennifer",
-    "publish_date": "2014-12-14",
-    "publisher": "No Starch Press",
-    "numOfPages": 472,
+let groupId = 3
+
+let groups = [{
+    "id": "1",
+    "admin_name": "jennifer",
+    "orders":[
+        {
+            "username": "anton",
+            "items": [{
+                "id": "1",
+                "url": "a.com",
+                "price": "13",
+            },
+            {
+                "id": "2",
+                "url": "b.com",
+                "price": "130",
+            }],
+        },
+        {
+            "username": "anton",
+            "items": [{
+                "id": "1",
+                "url": "a.com",
+                "price": "13",
+            },
+            {
+                "id": "2",
+                "url": "b.com",
+                "price": "130",
+            }],
+        }],
+
 },
 {
-    "isbn": "9781449331818",
-    "title": "Learning JavaScript Design Patterns",
-    "author": "Addy Osmani",
-    "publish_date": "2012-07-01",
-    "publisher": "O'Reilly Media",
-    "numOfPages": 254,
-},
-{
-    "isbn": "9781449365035",
-    "title": "Speaking JavaScript",
-    "author": "Axel Rauschmayer",
-    "publish_date": "2014-02-01",
-    "publisher": "O'Reilly Media",
-    "numOfPages": 460,
-}];
+    "id": "2",
+    "admin_name": "shi",
+    "orders":[
+        {
+            "username": "anton",
+            "items": [{
+                "id": "1",
+                "url": "a.com",
+                "price": "13",
+            },
+            {
+                "id": "2",
+                "url": "b.com",
+                "price": "130",
+            }],
+        },
+        {
+            "username": "anton",
+            "items": [{
+                "id": "1",
+                "url": "a.com",
+                "price": "13",
+            },
+            {
+                "id": "2",
+                "url": "b.com",
+                "price": "130",
+            }],
+        }],
+
+}
+
+];
+
 
 let users = [{
     "username": "jennifer",
     "password": "1234",
-    "power": "admin" 
+
 }, {
-    "username": "shivani",
+    "username": "shi",
     "password": "5678",
-    "power": "user" 
+
 }];
 
 var currentUser = null;
@@ -95,7 +140,7 @@ app.post('/login', (req, res) => {
             currentUser = userLogin
 
             //res.sendFile(__dirname + '/book-list.html');
-            res.redirect("http://localhost:8000/orders");
+            res.redirect("http://localhost:8000/groups/"+ userLogin);
             return;
         }
     }
@@ -103,15 +148,18 @@ app.post('/login', (req, res) => {
     res.send(`User Not Found; Username/Password Incorrect`);
 });
 
-app.get('/orders', (req, res) => {
+app.get('/groups/:username', (req, res) => {
+
+    const username = req.params.username;
+    
     for (let i of users) {
         if (i.username === currentUser) {
             if (i.power === "admin"){
-                res.sendFile(__dirname + '/book-list.html');
+                res.sendFile(__dirname + '/group-list.html');
 
             }
             else {
-                res.sendFile(__dirname + '/book-list.html');
+                res.sendFile(__dirname + '/group-list.html');
             }
 
             return;
@@ -122,8 +170,63 @@ app.get('/orders', (req, res) => {
     
 });
 
+// ********** Group FUNCTIONS *************
+//returns group with id
+app.get('/group/:id', (req, res) => {
+    // reading id from the URL
+    const id = req.params.id;
+
+    // searching books for the isbn
+    for (let group of groups) {
+        if (group.id === id) {
+            res.json(group);
+            return;
+        }
+    }
+
+    // sending 404 when not found something is a good practice
+    res.status(404).send('Book not found');
+});
+
+//adds a group
+app.post('/group', (req, res) => {
+    const group = req.body;
+
+    // output the book to the console for debugging
+    console.log(group);
+    books.push(group);
+
+    res.send('Group is added to the database');
+});
+
+
+//returns list of all groups
+app.get('/group', (req, res) => {
+    res.json(groups);
+});
+
+// Delets a group
+app.delete('/group/:id', (req, res) => {
+    // reading isbn from the URL
+    const id = req.params.id;
+
+    // remove item from the groups array
+    groups = groups.filter(i => {
+        if (i.id !== id) {
+            return true;
+        }
+
+        return false;
+    });
+
+
+    // sending 404 when not found something is a good practice
+    res.send('Group is deleted');
+});
+
 // ********** BOOK FUNCTIONS *************
-app.get('/book/:isbn', (req, res) => {
+//returns list of book with isbn
+app.get('/item/:id', (req, res) => {
     // reading isbn from the URL
     const isbn = req.params.isbn;
 
@@ -139,7 +242,7 @@ app.get('/book/:isbn', (req, res) => {
     res.status(404).send('Book not found');
 });
 
-
+// Adds a book
 app.post('/book', (req, res) => {
     const book = req.body;
 
@@ -150,11 +253,12 @@ app.post('/book', (req, res) => {
     res.send('Book is added to the database');
 });
 
+//returns list of all books
 app.get('/book', (req, res) => {
     res.json(books);
 });
 
-
+// Delets a book
 app.delete('/book/:isbn', (req, res) => {
     // reading isbn from the URL
     const isbn = req.params.isbn;
@@ -172,6 +276,7 @@ app.delete('/book/:isbn', (req, res) => {
     res.send('Book is deleted');
 });
 
+// Edits a book
 app.post('/book/:isbn', (req, res) => {
     // reading isbn from the URL
     const isbn = req.params.isbn;
