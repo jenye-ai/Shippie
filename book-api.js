0335bebd714223df34da7f4d923f2bd7816a7cfb
schidell <52084431+schidell@users.noreply.github.com>
2020-05-02 23:09:07 -0400

    Add files via upload
    working model!

--- book-api.js
+++ book-api.js
@@ -92,6 +92,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(express.static('11pm Shippie-master'));
+
 // HOME PAGE
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/home.html');
